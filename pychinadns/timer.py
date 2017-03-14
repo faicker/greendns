1368c0a4cf469b16c420e56feb01a3674a292214
faicker.mo <faicker.mo@gmail.com>
2017-03-14 12:38:04 +0800

    timer: Support priority queue not pop

--- pychinadns/timer.py
+++ pychinadns/timer.py
@@ -5,35 +5,36 @@ import Queue
 
 
 class Timer(object):
-    def __init__(self, now_ts, seconds, callback):
-        self.seconds = seconds
+    def __init__(self, now_ts, interval, callback):
+        self.interval = interval
         self.callback = callback
-        self.next_run_ts = now_ts + seconds
+        self.next_run_ts = now_ts + interval
 
     def __lt__(self, other):
         return self.next_run_ts < other.next_run_ts
 
     def Run(self):
         self.callback()
-        self.next_run_ts += self.seconds
+        self.next_run_ts += self.interval
 
 
 class TimerManager(object):
     def __init__(self):
         self.timers = Queue.PriorityQueue()
 
-    def AddTimer(self, seconds, callback):
-        t = Timer(int(time.time()), seconds, callback)
+    def AddTimer(self, interval, callback):
+        t = Timer(time.time(), interval, callback)
         self.timers.put(t)
 
     def CheckTimer(self):
+        now_ts = time.time()
         readd = []
         while not self.timers.empty():
-            t = self.timers.get()
-            readd.append(t)
-            if int(time.time()) >= t.next_run_ts:
-                t.Run()
-            else:
+            t = self.timers.queue[0]
+            if now_ts < t.next_run_ts:
                 break
+            self.timers.get()
+	    readd.append(t)
+	    t.Run()
         for t in readd:
             self.timers.put(t)
