b767715b5c9b201c7d4cf4b54b9c5c3fe14371b6
faicker.mo <faicker.mo@gmail.com>
2018-11-13 18:17:34 +0800

    Update chnroute.txt and Dockerfile
    Signed-off-by: faicker.mo <faicker.mo@gmail.com>

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-pychinadns -r chinadns -p ${LISTEN} -u ${IDNS},${ODNS} -l ${LOGLEVEL} -f /usr/local/etc/pychinadns/chnroute.txt -b /usr/local/etc/pychinadns/iplist.txt --cache --rfc1918
+cmd="pychinadns -r chinadns -p ${LISTEN} -u ${IDNS},${ODNS} -l ${LOGLEVEL} -t ${TIMEOUT} -f /usr/local/etc/pychinadns/chnroute.txt -b /usr/local/etc/pychinadns/iplist.txt"
+
+if [[ "${CACHE}" == "true" ]]; then
+    cmd="$cmd"" --cache"
+fi
+
+if [[ "$RFC1918" == "true" ]]; then
+    cmd="$cmd"" --rfc1918"
+fi
+
+$cmd
